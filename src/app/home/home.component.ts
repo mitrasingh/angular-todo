2b70a5e9920f4023bd6870875d01069dba43afde
mitrasingh <mitra.coder@gmail.com>
2024-11-12 20:26:32 -0500

    feat: add method for logging user input todo, apply event binding to form selector to receive output from child component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { TodoFormComponent } from './ui/todo-form.component';
-
+import { Todo } from '../shared/interfaces/todo';
 @Component({
   standalone: true,
   selector: 'app-home',
   template: `
     <h2>Todo</h2>
-    <app-todo-form />
+    <app-todo-form (todoSubmitted)="createTodo($event)" />
   `,
   imports: [TodoFormComponent],
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  createTodo(todo: Todo) {
+    console.log(todo);
+  }
+}
