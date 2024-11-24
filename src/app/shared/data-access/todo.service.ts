a4b58f6670694c96049da8618f3062a689e69189
mitrasingh <mitra.coder@gmail.com>
2024-11-24 18:27:10 -0500

    feat: add private and public property of todos, add method for adding todo to array

--- src/app/shared/data-access/todo.service.ts
+++ src/app/shared/data-access/todo.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Todo } from '../interfaces/todo';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TodoService {}
+export class TodoService {
+  // Property is private, only can be updated
+  #todos = signal<Todo[]>([]);
+
+  // Property can be ready publicly
+  todos = this.#todos.asReadonly();
+
+  addTodo(todo: Todo) {
+    this.#todos.update((todos) => [...todos, todo]);
+  }
+}
