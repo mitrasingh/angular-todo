9bb2a7786319991efe64f04514a7f42c5ce284b5
mitrasingh <mitra.coder@gmail.com>
2024-11-25 16:36:55 -0500

    feat: import and add service property to component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TodoFormComponent } from './ui/todo-form.component';
 import { Todo } from '../shared/interfaces/todo';
+import { TodoService } from '../shared/data-access/todo.service';
 @Component({
   standalone: true,
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { Todo } from '../shared/interfaces/todo';
   imports: [TodoFormComponent],
 })
 export default class HomeComponent {
+  todoService = inject(TodoService);
   createTodo(todo: Todo) {
     console.log(todo);
   }
