4ddb898963d34f30d4d84a7407c991230c20e5e9
mitrasingh <mitra.coder@gmail.com>
2024-11-26 09:20:42 -0500

    feat: import TodoListComponent, add selector to template, add property binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,16 @@ import { Component, inject } from '@angular/core';
 import { TodoFormComponent } from './ui/todo-form.component';
 import { Todo } from '../shared/interfaces/todo';
 import { TodoService } from '../shared/data-access/todo.service';
+import { TodoListComponent } from './ui/todo-list.component';
 @Component({
   standalone: true,
   selector: 'app-home',
   template: `
     <h2>Todo</h2>
     <app-todo-form (todoSubmitted)="todoService.addTodo($event)" />
+    <app-todo-list [todos]="todoService.todos()" />
   `,
-  imports: [TodoFormComponent],
+  imports: [TodoFormComponent, TodoListComponent],
 })
 export default class HomeComponent {
   todoService = inject(TodoService);
