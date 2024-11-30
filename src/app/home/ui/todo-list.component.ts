e147f6af4830844b4adcd0a3d78074508bbeb56b
mitrasingh <mitra.coder@gmail.com>
2024-11-29 20:42:26 -0500

    feat: import router link, add routerLink attribute of value dynamic route based on todo id

--- src/app/home/ui/todo-list.component.ts
+++ src/app/home/ui/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, input } from '@angular/core';
 import { Todo } from '../../shared/interfaces/todo';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -8,13 +9,14 @@ import { Todo } from '../../shared/interfaces/todo';
     <ul>
       @for (todo of todos(); track todo.id) {
       <li>
-        <a>{{ todo.title }}</a>
+        <a routerLink="/detail/{{ todo.id }}">{{ todo.title }}</a>
       </li>
       } @empty {
       <li>Nothing to do!</li>
       }
     </ul>
   `,
+  imports: [RouterLink],
 })
 export class TodoListComponent {
   todos = input.required<Todo[]>();
