6e348552372d72ed80e1584c70f2f4c383c681bb
mitrasingh <mitra.coder@gmail.com>
2024-12-03 13:50:39 -0500

    feat: create metadata and structure to component

---
+++ src/app/detail/ui/edit-todo-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-edit-todo-form',
+  template: `
+    <h1>edit</h1>
+    <button>Update Todo</button>
+  `,
+})
+export class EditTodoFormComponent {}
