03c3d71527b3c58458e041255a6ccec7037c989c
mitrasingh <mitra.coder@gmail.com>
2024-12-03 14:16:08 -0500

    feat: add output signal for form values, add form imports and properties/attributes to template

--- src/app/detail/ui/edit-todo-form.component.ts
+++ src/app/detail/ui/edit-todo-form.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { CreateTodo } from '../../shared/interfaces/todo';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-edit-todo-form',
   template: `
     <h1>edit</h1>
-    <button>Update Todo</button>
+    <form
+      [formGroup]="editForm"
+      (ngSubmit)="todoEdited.emit(editForm.getRawValue())"
+    >
+      <input />
+      <input />
+      <button>Update Todo</button>
+    </form>
   `,
+  imports: [ReactiveFormsModule],
 })
-export class EditTodoFormComponent {}
+export class EditTodoFormComponent {
+  todoEdited = output<CreateTodo>();
+  private fb = inject(FormBuilder);
+
+  editForm = this.fb.nonNullable.group({
+    title: [''],
+    description: [''],
+  });
+}
