7491c63418852479b3bd43287f632df75fe38323
mitrasingh <mitra.coder@gmail.com>
2024-12-10 15:34:52 -0500

    feat: remove if control flow, add properties for todoObj values, load todoObj values to class property values upon initial life cycle

--- src/app/detail/ui/edit-todo-form.component.ts
+++ src/app/detail/ui/edit-todo-form.component.ts
@@ -7,35 +7,44 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
   selector: 'app-edit-todo-form',
   template: `
     <h1>edit</h1>
-    @if (todoObj(); as todo) {
     <form
       [formGroup]="editForm"
       (ngSubmit)="todoEdited.emit(editForm.getRawValue())"
     >
-      <input
-        type="text"
-        formControlName="title"
-        placeholder="{{ todo.title }}"
-      />
+      <input type="text" formControlName="title" placeholder="new title" />
       <input
         type="text"
         formControlName="description"
-        placeholder="{{ todo.description }}"
+        placeholder="new description"
       />
+      <p>{{ titleValue }}</p>
       <button>Update Todo</button>
     </form>
-    }
   `,
   imports: [ReactiveFormsModule],
 })
 export class EditTodoFormComponent {
+  ngOnInit() {
+    this.idValue = this.todoObj()!.id;
+    this.titleValue = this.todoObj()!.title;
+    this.descriptionValue = this.todoObj()!.description;
+    this.editForm.patchValue({
+      id: this.idValue,
+      title: this.titleValue,
+      description: this.descriptionValue,
+    });
+  }
   private fb = inject(FormBuilder);
 
-  todoEdited = output<CreateTodo>();
+  todoEdited = output<Todo>();
   todoObj = input<Todo>();
+  idValue = '';
+  titleValue = '';
+  descriptionValue = '';
 
   editForm = this.fb.nonNullable.group({
-    title: [''],
-    description: [''],
+    id: [this.idValue],
+    title: [this.titleValue],
+    description: [this.descriptionValue],
   });
 }
